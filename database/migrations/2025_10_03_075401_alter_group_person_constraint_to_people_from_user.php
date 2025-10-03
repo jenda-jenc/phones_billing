0d807bf3304f9fc58defbe7c56a5935375c9f84f
cernohorsky <cernohorskyj@senat.cz>
2025-10-03 11:09:50 +0200

    fix migrations

---
+++ database/migrations/2025_10_03_075401_alter_group_person_constraint_to_people_from_user.php
@@ -0,0 +1,26 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('group_person', function (Blueprint $table) {
+            // zrušení starého FK na users
+            $table->dropForeign('group_user_user_id_foreign');
+
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('group_person', function (Blueprint $table) {
+            // obnovení FK na users
+            $table->foreign('person_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+};
